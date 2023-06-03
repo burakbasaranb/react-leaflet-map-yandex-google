723445391b39b01c1565d7600025df921c1298e3
kuzgunisiyah <kuzgunisiyah@gmail.com>
2023-06-03 18:30:41 +0300

    bb

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 
 import { Map as LeafletMap, TileLayer, Marker, Popup, LayersControl} from "react-leaflet";
-import 'leaflet/dist/leaflet.css' 
-import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'; 
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
